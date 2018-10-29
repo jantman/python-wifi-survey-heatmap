5259c4fc6cf3f000967ed6c5b5f886f021e605a6
Jason Antman <jason@jasonantman.com>
2018-10-28 20:55:21 -0400

    simplify

--- wifi_survey_heatmap/heatmap.py
+++ wifi_survey_heatmap/heatmap.py
@@ -75,13 +75,10 @@ class HeatMapGenerator(object):
     def generate(self):
         layout = imread(self._image_path)
         s_beacons = ['2e:20', 'f6:70', '5b:30', '74:c0', 'f5:90', '16:a0']
-        g_beacons = ['14:a1', 'f6:71', '5b:31', '74:c1', 'f5:91', '16:a1']
         a = defaultdict(list)
-        self._rows = []
         with open('%s.csv' % self._title, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                self._rows.append(row)
                 rssis = []
                 for k, v in row.items():
                     a[k].append(int(v))
@@ -97,23 +94,10 @@ class HeatMapGenerator(object):
 
         x = np.linspace(0, grid_width, num_x)
         y = np.linspace(0, grid_height, num_y)
-
         gx, gy = np.meshgrid(x, y)
         gx, gy = gx.flatten(), gy.flatten()
-
-        levels = [-85, -80, -75, -70, -65, -60, -55, -50, -45, -40, -35, -30,
-                  -25]
-
-        interpolate = True
-        """
-        self._grid_plots(
-            s_beacons, interpolate, gx, gy, num_x, num_y, a, x, y, levels,
-            layout
-        )
-        """
         self._max_plot(
-            a, s_beacons, interpolate, gx, gy, num_x, num_y, x, y, levels,
-            layout
+            a, 'max_rssi', 'Max RSSI', gx, gy, num_x, num_y, layout
         )
 
     def _add_inner_title(self, ax, title, loc, size=None, **kwargs):
@@ -130,94 +114,29 @@ class HeatMapGenerator(object):
         )
         return at
 
-    def _grid_plots(
-        self, s_beacons, interpolate, gx, gy, num_x, num_y, a, x, y, levels,
-            layout
-    ):
-        f = pp.figure()
-        f.suptitle("Individual AP RSSI")
-        # Adjust the margins and padding
-        f.subplots_adjust(
-            hspace=0.1, wspace=0.1, left=0.05, right=0.95, top=0.85,
-            bottom=0.15
+    def _max_plot(self, a, key, title, gx, gy, num_x, num_y, layout):
+        pp.rcParams['figure.figsize'] = (
+            self._image_width / 300, self._image_height / 300
         )
-
-        # Create a grid of subplots using the AxesGrid helper
-        image_grid = AxesGrid(
-            f, 111, nrows_ncols=(2, 3), axes_pad=0.1, label_mode="1",
-            share_all=True, cbar_location="right", cbar_mode="single",
-            cbar_size="3%"
+        pp.title(title)
+        # Interpolate the data
+        rbf = Rbf(
+            a['Drawing X'], a['Drawing Y'], a[key], function='linear'
         )
-
-        for beacon, i in zip(s_beacons, range(len(s_beacons))):
-            # Hide the axis labels
-            image_grid[i].xaxis.set_visible(False)
-            image_grid[i].yaxis.set_visible(False)
-
-            if interpolate:
-                # Interpolate the data
-                rbf = Rbf(
-                    a['Drawing X'], a['Drawing Y'], a[beacon], function='linear'
-                )
-                z = rbf(gx, gy)
-                z = z.reshape((num_y, num_x))
-
-                # Render the interpolated data to the plot
-                image = image_grid[i].imshow(
-                    z, vmin=-85, vmax=-25,
-                    extent=(0, self._image_width, self._image_height, 0),
-                    cmap='RdYlBu_r', alpha=1
-                )
-                # c = image_grid[i].contourf(z, levels, alpha=0.5)
-                # c = image_grid[i].contour(z, levels, linewidths=5, alpha=0.5)
-            else:
-                z = ml.griddata(
-                    a['Drawing X'], a['Drawing Y'], a[beacon], x, y
-                )
-                c = image_grid[i].contourf(x, y, z, levels, alpha=0.5)
-            image_grid[i].imshow(
-                layout, interpolation='bicubic', zorder=100
-            )
-
-        # Setup the data for the colorbar and its ticks
-        image_grid.cbar_axes[0].colorbar(image)
-        image_grid.cbar_axes[0].set_yticks(levels)
-
-        # Add inset titles to each subplot
-        for ax, im_title in zip(image_grid, s_beacons):
-            t = self._add_inner_title(ax, "Beacon %s" % im_title, loc=3)
-            t.patch.set_alpha(0.5)
-        pp.show()
-
-    def _max_plot(
-        self, a, s_beacons, interpolate, gx, gy, num_x, num_y, x, y, levels,
-            layout
-    ):
-        max_rssi = a['max_rssi']
-        logger.debug('Max RSSIs: %s', a['max_rssi'])
-        pp.title("Maximum RSSI seen for each beacon")
-        if interpolate:
-            # Interpolate the data
-            rbf = Rbf(
-                a['Drawing X'], a['Drawing Y'], max_rssi, function='linear'
-            )
-            z = rbf(gx, gy)
-            z = z.reshape((num_y, num_x))
-            # Render the interpolated data to the plot
-            image = pp.imshow(
-                z, vmin=-85, vmax=-25,
-                extent=(0, self._image_width, self._image_height, 0),
-                cmap='RdYlBu_r', alpha=1
-            )
-            # pp.contourf(z, levels, alpha=0.5)
-            # pp.contour(z, levels, linewidths=5, alpha=0.5)
-        else:
-            z = ml.griddata(a['Drawing X'], a['Drawing Y'], max_rssi, x, y)
-            pp.contourf(x, y, z, levels, alpha=0.5)
+        z = rbf(gx, gy)
+        z = z.reshape((num_y, num_x))
+        # Render the interpolated data to the plot
+        pp.axis('off')
+        image = pp.imshow(
+            z, vmin=-85, vmax=-25,
+            extent=(0, self._image_width, self._image_height, 0),
+            cmap='RdYlBu_r', alpha=1
+        )
+        # pp.contourf(z, levels, alpha=0.5)
+        # pp.contour(z, levels, linewidths=5, alpha=0.5)
         pp.colorbar(image)
         pp.imshow(layout, interpolation='bicubic', zorder=100)
-        logger.debug('Size in inches=%s', pp.get_size_inches())
-        pp.savefig('max_%s.png' % self._title)
+        pp.savefig('%s_%s.png' % (key, self._title), dpi=300)
 
 
 def parse_args(argv):

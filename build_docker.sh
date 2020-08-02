23429a4fcdc9ba68a3f4d3d9c75c29c4c07b000c
Jason Antman <jason@jasonantman.com>
2020-08-02 14:19:14 -0400

    fix docker build script

--- build_docker.sh
+++ build_docker.sh
@@ -3,7 +3,7 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 BDATE=$(date +%Y-%m-%dT%H:%M:%S.00%Z)
 REF=$(git rev-parse --short HEAD)
-if git diff --no-ext-diff --quiet --exit-code || git diff-index --cached --quiet HEAD --; then
+if ! git diff --no-ext-diff --quiet --exit-code || ! git diff-index --cached --quiet HEAD --; then
   REF="${REF}-dirty"
 fi
 
